3e715ec59cd4b62a7130083577f338f7bf349ff1
sifact <sifatami596@gmail.com>
2023-07-29 15:32:04 +0600

    featured done

--- components/UI/FeaturedProducts.jsx
+++ components/UI/FeaturedProducts.jsx
@@ -1,16 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Header from "./Header";
 
 const FeaturedProducts = ({ products }) => {
   console.log(products);
   return (
     <div>
-      <div className="my-32 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-12 container mx-auto">
+      <Header
+        title="Featured Products"
+        subtitle="Discover our top-rated products crafted with care"
+      />
+      <div className="my-32 grid lg:grid-cols-3 md:grid-cols-2 gap-12 container mx-auto">
         {products ? (
-          products.map((product) => (
-            <FeaturedProduct key={product._id} product={product} />
-          ))
+          products.map(
+            (product, idx) =>
+              idx < 6 && <FeaturedProduct key={product._id} product={product} />
+          )
         ) : (
           <>Loading...</>
         )}
@@ -24,29 +30,31 @@ export default FeaturedProducts;
 const FeaturedProduct = ({ product }) => {
   const { _id, title, img, category, price, status } = product;
   return (
-    <div className="relative space-y-6">
-      <div className="relative h-[300px]  bg-gradient-to-tr from-gray-900 to-green-50 rounded-lg">
-        <img
-          className="object-cover w-full h-full rounded-lg mix-blend-overlay"
-          src={img}
-          alt=""
-        />
-      </div>
-
-      <Link href={`/bookDetails/${_id}`}>
-        <div className="rounded-lg space-y-3 ">
-          <div className="flex justify-between items-center">
-            <h1 className="text-xl font-semibold">{title}</h1>
-            <p className="opacity-60 text-xl">{category}</p>
+    <Link href={`/productDetails/${_id}`}>
+      <div className="shadow-md bg-gray-100 h-auto rounded-md p-4">
+        <div className="h-[300px]">
+          <img
+            className="rounded-lg object-cover w-full h-full"
+            src={img ? img : peperoni}
+            alt="pizza"
+          />
+        </div>
+        <div>
+          <div className="text-center">
+            <h2 className="text-lg font-bold py-2 ">{title}</h2>
+            <span className="bg-gray-200 rounded-full text-sm px-4">
+              {category}
+            </span>
           </div>
-          <div className="opacity-60 font-semibold flex justify-between">
-            <div>
-              <p>{price}</p>
-              <p>{status}</p>
-            </div>
+
+          <div className="flex justify-between items-center mt-4">
+            <span className="font-semibold">Tk {price}</span>
+            <button className="bg-amber-500 py-2 px-3 rounded-full">
+              {status ? "In Stock" : "Out of stock"}
+            </button>
           </div>
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
