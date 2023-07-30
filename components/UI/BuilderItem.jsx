e04ce29ce81cb0105f31dac2e6400da0fa4426c6
sifact <sifatami596@gmail.com>
2023-07-30 17:28:28 +0600

    pc builder page

---
+++ components/UI/BuilderItem.jsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from "react";
+
+const BuilderItem = ({ category }) => {
+  return (
+    <>
+      <div className="flex justify-between my-4 items-center">
+        <div className="flex gap-4 items-center">
+          <div className="w-[100px] h-[100px]  ">
+            <img
+              className="object-cover w-full h-full rounded-md"
+              src={category.img}
+              alt=""
+            />
+          </div>
+          <span>{category.title}</span>
+        </div>
+        <div>
+          <Link href={`/categories/${category.title}`}>
+            <button className="px-2 py-1 bg-amber-500 rounded-bl-[15%] text-white rounded-sm ">
+              Choose
+            </button>
+          </Link>
+        </div>
+      </div>
+      <hr />
+    </>
+  );
+};
+
+export default BuilderItem;
--- pages/pcBuilder/index.jsx
+++ pages/pcBuilder/index.jsx
@@ -1,12 +1,39 @@
 import RootLayout from "@/components/Layouts/RootLayout";
+import BuilderItem from "@/components/UI/BuilderItem";
+import Header from "@/components/UI/Header";
 import React from "react";
 
-const PcBuilder = () => {
-  return <div>PcBuilder</div>;
+const PcBuilder = ({ categories }) => {
+  return (
+    <div className="container mx-auto flex justify-center">
+      <div className=" md:w-[70%] mb-32 mt-20 p-8 shadow-sm ">
+        <Header
+          title="Build Your PC"
+          subtitle="Explore our curated selection of top categories"
+        />
+        {categories.map((category) => (
+          <BuilderItem category={category} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PcBuilder;
 
 PcBuilder.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/api/category");
+  const data = await res.json();
+  console.log(data);
+
+  return {
+    props: {
+      categories: data,
+    },
+    revalidate: 10,
+  };
 };
